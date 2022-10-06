d54d13606471c004c16826f2448dac175761296c
BorisSolomonia <78170135+BorisSolomonia@users.noreply.github.com>
2022-10-06 17:48:59 +0400

    Update DeveloperProjectFinder.java

--- src/main/java/com/epam/mjc/collections/combined/DeveloperProjectFinder.java
+++ src/main/java/com/epam/mjc/collections/combined/DeveloperProjectFinder.java
@@ -1,10 +1,21 @@
 package com.epam.mjc.collections.combined;
 
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
+package com.epam.mjc.collections.combined;
+
+import java.util.*;
 
 public class DeveloperProjectFinder {
     public List<String> findDeveloperProject(Map<String, Set<String>> projects, String developer) {
+        List<String> sortList = new ArrayList<>();
+        PriorityQueue<String> sortPrior = new PriorityQueue<>(Comparator.reverseOrder());
+        for (String k : projects.keySet()) {
+            if (projects.get(k).contains(developer)){
+                sortPrior.add(k);
+            }
+        }
+        for (String n : sortPrior) {
+            sortList.add(sortPrior.poll());
+        }
+        return sortList;
     }
 }
